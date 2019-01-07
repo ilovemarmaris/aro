fb4ef2e8c05e354037e0781edb856546ef83d342
ilovemarmaris <amandagates80@gmail.com>
2019-01-07 16:45:20 +0300

    tolga

--- run.sh
+++ run.sh
@@ -1,34 +1,11 @@
 #!/bin/bash
-# create a folder to put the miner in, and go inside it
-mkdir arion
-cd arion
-
-# download latest binary archive, make sure that you use the correct url for your Ubuntu version (16, 17, 18 ...)
-wget -O arionum-gpu-miner.tar.gz "https://bitbucket.org/cryptogone/arionum-gpu-miner/downloads/arionum-gpu-miner-v1.5.1-ubuntu16-cuda9.0.tar.gz"
-
-# extract archive 
-tar xzvf arionum-gpu-miner.tar.gz
-
-worker=`hostname`
-pool="http://mine.arionumpool.com"
-wallet="3oBrsyMm6P7iwNrGd6yx9NXpWu4sdBKJjtASan9WKT3yKyhE3PjViyXcsy6GLzukGsLRTj5ixNKxXLnQkGEnSUXz"
-threads="3"
-batches="230"
-
-# set this to false if you do not want miner to auto relaunch after crash
-relaunch_miner_on_crash="true"
-
-while :
-do
-	# -u means use all device, you can also use -d to specify list of devices (ex: -d 0,2,5)
-	./arionum-cuda-miner -u -b "$batches" -t "$threads" -p "$pool" -a "$wallet" -i "$worker"
-
-	if [ "$relaunch_miner_on_crash" = "true" ]; then
-		echo "miner crashed, relaunching in 5 seconds ..."
-		sleep 5
-	else
-		break
-	fi
-done
-
+sudo apt-get update;
+sudo apt-get -y install git cmake gcc g++ ocl-icd-opencl-dev;
+git clone http://github.com/bogdanadnan/ariominer.git;
+cd ariominer;
+mkdir build;
+cd build;
+cmake ..
+make
+./ariominer --mode miner --pool http://mine.arionumpool.com --wallet 2EWzXdsPh1W9WEYtMuuubdSzvcPsM4PJQjN7viEyYRWvjSEs7noPF4T58kkb77bnR4QEnddUh7MDUV6pWADhueQL --name tolga
 
