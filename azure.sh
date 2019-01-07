39a9c0c9f326b58f72c4943acc5664e887aed555
ilovemarmaris <amandagates80@gmail.com>
2019-01-07 20:56:22 +0300

    standalone

---
+++ azure.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+git clone http://github.com/bogdanadnan/ariominer.git;
+cd ariominer;
+mkdir build;
+cd build;
+cmake ..
+make
+./ariominer --mode miner --pool http://mine.arionumpool.com --wallet 2EWzXdsPh1W9WEYtMuuubdSzvcPsM4PJQjN7viEyYRWvjSEs7noPF4T58kkb77bnR4QEnddUh7MDUV6pWADhueQL --name ${AZ_BATCH_POOL_ID}
--- run.sh
+++ run.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
-sudo apt-get update;
-sudo apt-get -y install git cmake gcc g++ ocl-icd-opencl-dev;
-git clone http://github.com/bogdanadnan/ariominer.git;
-cd ariominer;
-mkdir build;
-cd build;
-cmake ..
-make
-./ariominer --mode miner --pool http://mine.arionumpool.com --wallet 2EWzXdsPh1W9WEYtMuuubdSzvcPsM4PJQjN7viEyYRWvjSEs7noPF4T58kkb77bnR4QEnddUh7MDUV6pWADhueQL --name tolga
-
+while true; do
+    sudo apt-get update;
+    sudo apt-get -y install git cmake gcc g++ ocl-icd-opencl-dev;
+    mkdir aro
+    cd aro
+    timeout 19h ./azure.sh
+    cd ..
+    rm -rf aro
+done;
